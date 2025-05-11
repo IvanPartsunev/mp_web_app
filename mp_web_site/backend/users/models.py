f420ac45f22349e3271ade7571962f887ba962f9
ivanparcunev <ivan.partsunev@linkin.eu>
2025-05-11 18:34:11 +0300

    refactor: rename directory from mp_web_site to backend/users

--- mp_web_site/backend/users/models.py
+++ mp_web_site/backend/users/models.py
@@ -3,17 +3,16 @@ from typing import List, Optional
 from datetime import datetime
 from uuid import uuid4
 
-from mp_web_site.backend.modules.users.roles import UserRole
+from mp_web_site.backend.users.roles import UserRole
 
 
 class UserBase(BaseModel):
     email: EmailStr
     phone: int
-    role: List[str] = [UserRole.REGULAR_USER]
-    active: bool = False
-
 
 class UserCreate(UserBase):
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
     password: str
 
 
@@ -28,9 +27,14 @@ class UserUpdate(BaseModel):
 class UserInDB(UserBase):
     id: str = Field(default_factory=lambda: str(uuid4()))
     user_code: Optional[str] = None
+    role: List[str] = [UserRole.REGULAR_USER]
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now)
 
 
 class User(UserInDB):
-    pass
+    id: str
+    user_code: Optional[str] = None
+    role: List[str]
+    created_at: datetime
+    updated_at: datetime
