6a13e6b1cf99ada77024958cdf22a0aa02c1d6a6
ivanparcunev <ivan.parcunev@gmail.com>
2025-05-25 01:09:51 +0300

    fix: update router imports and include auth router in API

--- mp_web_site/backend/api.py
+++ mp_web_site/backend/api.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
-from mp_web_site.backend.users import routers
+from mp_web_site.backend.users.routers import user_router
+from mp_web_site.backend.auth.routers import auth_router
 
 app = FastAPI(docs_url="/api/docs", redoc_url="/api/redoc")
 
-app.include_router(routers.user_router, prefix="/api/users")
+app.include_router(user_router, prefix="/api/users")
+app.include_router(auth_router, prefix="/api/users")
