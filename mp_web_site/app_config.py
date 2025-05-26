9f57d459e5ec61d961a58ebfd64f490a62117f66
ivanparcunev <ivan.parcunev@gmail.com>
2025-05-26 13:53:26 +0300

    feat: add SesSettings class for email configuration with AWS SES

--- mp_web_site/app_config.py
+++ mp_web_site/app_config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 
+REGION = "eu-central-1"
 
 class DynamoDBSettings(BaseSettings):
   """DynamoDB configuration settings."""
@@ -13,11 +14,22 @@ class DynamoDBSettings(BaseSettings):
     env_file = ".env"
     extra = "ignore"
 
+
 class JWTSettings(BaseSettings):
   secret_key: str
   algorithm: str
 
   class Config:
     env_prefix = "JWT_"
+    env_file = ".env"
+    extra = "ignore"
+
+
+class SesSettings(BaseSettings):
+  sender: str
+  region: str = REGION
+
+  class Config:
+    env_prefix = "MAIL_"
     env_file = ".env"
     extra = "ignore"
