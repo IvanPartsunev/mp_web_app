c4e20e7ab8e01f295ca81523566ece53524e2808
ivanparcunev <ivan.parcunev@gmail.com>
2025-06-28 19:59:04 +0300

    refactor: standardize code formatting and fix shadcn/ui import paths

--- mp_web_app/frontend/src/App.tsx
+++ mp_web_app/frontend/src/App.tsx
@@ -1,22 +1,17 @@
 import React from "react";
-import {ThemeProvider} from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
 
-import { Button } from "@shadcn/ui/button";
+import { Button } from "@/components/ui/button";
 
 function App() {
-    return (
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-
-            <div className="flex flex-col items-center justify-center min-h-screen">
-                <h1 className="text-3xl font-bold underline mb-4">
-                    Hello world!
-                </h1>
-                <Button>
-                    Click me
-                </Button>
-            </div>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-3xl font-bold underline mb-4">Hello world!</h1>
+        <Button>Click me</Button>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- mp_web_app/frontend/vite.config.mts
+++ mp_web_app/frontend/vite.config.mts
@@ -1,15 +1,18 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import * as path from "path";
 
 // @ts-ignore
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
   },
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
+});
