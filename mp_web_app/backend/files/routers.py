a156ccb26c15101d13822f9787d4518f4d5b1e59
IvanPartsunev <ivan.parcunev@gmail.com>
2025-08-30 20:45:42 +0300

    feature: create upload file endpoint

--- mp_web_app/backend/files/routers.py
+++ mp_web_app/backend/files/routers.py
@@ -1,9 +1,29 @@
-from fastapi import APIRouter, HTTPException
+from typing import List
+
+from fastapi import APIRouter, HTTPException, UploadFile, File, Depends, Form
+from starlette import status
+
+from auth.operations import role_required
+from database.operations import UploadsRepository
+from files.models import FileMetadata, FileType
+from files.operations import upload_to_s3, get_uploads_repository
+from users.operations import get_user_repository
+from users.roles import UserRole
 
 file_router = APIRouter(tags=["files"])
 
-@file_router.post("/upload")
-async def upload_files():
+@file_router.post("/upload", status_code=status.HTTP_201_CREATED)
+async def upload_files(
+    file_name: str = Form(...),
+    file_type: FileType = Form(...),
+    allowed_to: List[str] = Form([]),
+    file: UploadFile = File(...),
+    repo: UploadsRepository = Depends(get_uploads_repository),
+    # user=Depends(role_required([UserRole.REGULAR_USER])) #TODO Change this to Admin
+):
+  file_metadata = FileMetadata(file_name=file_name, file_type=file_type, allowed_to=allowed_to)
+  file = file
+  upload_to_s3(file_metadata, file, repo)
   return HTTPException(status_code=204, detail="Not implemented")
 
 
