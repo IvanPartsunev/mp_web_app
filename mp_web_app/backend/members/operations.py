1f53e04e1f2dfa16dc06cd838ec06b49561131ec
IvanPartsunev <ivan.parcunev@gmail.com>
2025-11-11 23:40:41 +0200

    feat: enhance member management with sync and CSV support
    - Renamed `members_upload` API endpoint to `sync_members_list` for clarity.
    - Added CSV file support for member list uploads, replacing the previous PDF validation.
    - Implemented functions for CSV parsing, normalization, and syncing members in the database.
    - Improved code structure by splitting logic into reusable functions and handling bulk updates/deletions efficiently.
    - Updated models and validation to ensure data integrity during synchronization.

--- mp_web_app/backend/members/operations.py
+++ mp_web_app/backend/members/operations.py
@@ -1,13 +1,17 @@
+import csv
+import io
 import os
+from typing import Any
 
 from botocore.exceptions import ClientError
 from fastapi import HTTPException
+from starlette.datastructures import UploadFile
 
 from database.repositories import MemberRepository
-from users.models import Member
+from members.models import Member
 from users.operations import validate_email, validate_phone
 
-MEMBERS_TABLE_NAME = os.environ.get('MEMBERS_TABLE_NAME')
+MEMBERS_TABLE_NAME = os.environ.get("MEMBERS_TABLE_NAME")
 
 
 def get_member_repository() -> MemberRepository:
@@ -18,44 +22,119 @@ def get_member_repository() -> MemberRepository:
 def is_member_code_valid(member_code: str, repo: MemberRepository) -> Member | None:
   response = repo.table.get_item(Key={"member_code": member_code})
 
-  if 'Item' not in response or not response['Item'].get('is_valid', None):
+  if "Item" not in response or not response["Item"].get("member_code_valid", None):
     return None
-  return repo.convert_item_to_object(response['Item'])
+  return repo.convert_item_to_object(response["Item"])
 
 
 def create_member(member: Member, repo: MemberRepository):
-
   member_item = {
-    'first_name': member.first_name,
-    'last_name': member.last_name,
-    'email': validate_email(member.email) if member.email else None,
-    'phone': validate_phone(member.phone) if member.phone else None,
-    'proxy': member.proxy,
-    'member_code': member.member_code,
-    'member_code_valid': True,
+    "first_name": member.first_name,
+    "last_name": member.last_name,
+    "email": validate_email(member.email) if member.email else None,
+    "phone": validate_phone(member.phone) if member.phone else None,
+    "proxy": member.proxy,
+    "member_code": member.member_code,
+    "member_code_valid": True,
   }
   try:
     repo.table.put_item(Item=member_item)
   except ClientError as e:
-    raise HTTPException(
-      status_code=500,
-      detail=f"Database error: {e.response['Error']['Message']}"
-    )
+    raise HTTPException(status_code=500, detail=f"Database error: {e.response['Error']['Message']}")
+
+
+async def convert_members_list(file: UploadFile) -> list[dict[str, Any]]:
+  contents = await file.read()
+  decoded = contents.decode("utf-8")
+  reader = csv.DictReader(io.StringIO(decoded))
+  return list(reader)
+
+
+def sync_members_list(new_members_list: list[dict[str, Any]], repo: MemberRepository):
+  """
+  Sync persons while preserving existing member codes and their states.
+  - Add new persons
+  - Remove persons not in the new list
+  - Keep existing persons with their code states intact
+  """
+  table = repo.table
+  existing_members = _get_members_from_db(repo)
+  new_members_list = _normalize_members(new_members_list)
+
+  if not existing_members:
+    with table.batch_writer() as batch:
+      for member in new_members_list:
+        batch.put_item(Item=member)
+    return
+
+  current_members_codes = {em["member_code"]: em for em in existing_members}
+  new_members_codes = {nm["member_code"] for nm in new_members_list}
+
+  to_remove = set(current_members_codes.keys()) - new_members_codes
+  to_add = new_members_codes - set(current_members_codes.keys())
+
+  with table.batch_writer() as batch:
+    for member_code in to_remove:
+      batch.delete_item(Key={"member_code": member_code})
+
+    for member in new_members_list:
+      if member["member_code"] in to_add:
+        batch.put_item(Item=member)
 
 
 def update_member_code(member_code: str, repo: MemberRepository) -> None:
   response = repo.table.get_item(Key={"member_code": member_code})
   member_obj = repo.convert_item_to_object(response["Item"])
   response = repo.table.update_item(
-    Key={'member_code': member_obj.member_code},
-    UpdateExpression='SET #is_valid = :is_valid',
-    ExpressionAttributeNames={'#is_valid': 'is_valid'},
-    ExpressionAttributeValues={':is_valid': not member_obj.is_valid},
-    ReturnValues="ALL_NEW"
+    Key={"member_code": member_obj.member_code},
+    UpdateExpression="SET #member_code_valid = :member_code_valid",
+    ExpressionAttributeNames={"#member_code_valid": "member_code_valid"},
+    ExpressionAttributeValues={":member_code_valid": not member_obj.member_code_valid},
+    ReturnValues="ALL_NEW",
   )
   return repo.convert_item_to_object(response["Attributes"])
 
 
 def is_valid_file_type(file_name: str):
-  if not file_name.endswith(".pdf"):
-    raise ValueError("Invalid members list file type. Allowed type: ['.pdf']")
+  if not file_name.endswith(".csv"):
+    raise ValueError("Invalid members list file type. Allowed type: ['.csv']")
+
+
+def _normalize_members(new_members_list: list[dict[str, Any]]):
+  normalized_members_list = []
+
+  for member in new_members_list:
+    try:
+      validate_phone(member["phone"])
+      validate_email(member["email"])
+    except ValueError:
+      member["phone"] = None
+      member["email"] = None
+
+    normalized_members_list.append(
+      {
+        "first_name": member["first_name"].capitalize(),
+        "last_name": member["last_name"].capitalize(),
+        "phone": member["phone"] if member["phone"] else None,
+        "email": member["email"] if member["email"] else None,
+        "member_code": member["member_code"],
+        "member_code_valid": True,
+      }
+    )
+  return normalized_members_list
+
+
+def _get_members_from_db(repo: MemberRepository) -> list[dict[str, Any]]:
+  members = []
+  scan_kwargs = {}
+  table = repo.table
+
+  while True:
+    response = table.scan(**scan_kwargs)
+    members.extend(response.get("Items", []))
+
+    if "LastEvaluatedKey" not in response:
+      break
+    scan_kwargs["ExclusiveStartKey"] = response["LastEvaluatedKey"]
+
+  return members
--- mp_web_app/backend/members/routers.py
+++ mp_web_app/backend/members/routers.py
@@ -1,18 +1,25 @@
-from fastapi import APIRouter, status, Depends, HTTPException, UploadFile, File
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, status
 
 from auth.operations import role_required
 from database.repositories import MemberRepository
-from members.operations import get_member_repository, create_member, is_valid_file_type
-from users.models import Member
+from members.models import Member
+from members.operations import (
+  convert_members_list,
+  create_member,
+  get_member_repository,
+  is_valid_file_type,
+  sync_members_list,
+)
 from users.roles import UserRole
 
-member_router = APIRouter(tags=['member'])
+member_router = APIRouter(tags=["member"])
+
 
 @member_router.post("/create_members", status_code=status.HTTP_201_CREATED)
 async def member_create(
   member: Member,
   member_repo: MemberRepository = Depends(get_member_repository),
-  user=Depends(role_required([UserRole.ADMIN]))
+  user=Depends(role_required([UserRole.ADMIN])),
 ):
   try:
     create_member(member, member_repo)
@@ -20,14 +27,16 @@ async def member_create(
     raise HTTPException(status_code=400, detail=str(e))
 
 
-@member_router.post("/upload_members_list", status_code=status.HTTP_200_OK)
+@member_router.post("/sync_members_list", status_code=status.HTTP_200_OK)
 async def members_upload(
   file: UploadFile,
+  member_repo: MemberRepository = Depends(get_member_repository),
   # user = Depends(role_required([UserRole.ADMIN]))
 ):
-
   try:
     file_name = file.filename
     is_valid_file_type(file_name)
+    data = await convert_members_list(file)
+    sync_members_list(data, member_repo)
   except ValueError as e:
     raise HTTPException(status_code=400, detail=str(e))
